8bbf62e468296dd963a91b7662b4db376e1e5988
arthur andrade <arthurandrad.23@gmail.com>
2024-06-10 20:25:27 -0300

    Feat: Implemented Connection to Database to savemessages

--- app.js
+++ app.js
@@ -1,43 +1,79 @@
 const express = require('express');
 const http = require('http');
 const socketIo = require('socket.io');
+const mongoose = require('mongoose');
 
 const app = express();
 const server = http.createServer(app);
 const io = socketIo(server);
 
+// MongoDB connection
+mongoose.connect('mongodb://localhost:27017/ChatApp', {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+});
+
+const { Schema } = mongoose;
+
+// Define user schema
+const userSchema = new Schema({
+    username: String,
+    profilePicture: Buffer // Or you can store the path to the profile picture
+});
+
+// Define message schema
+const messageSchema = new Schema({
+    sender: String,
+    recipient: String,
+    message: String,
+    timestamp: String,
+    roomId: String
+});
+
+// Create models
+const User = mongoose.model('User', userSchema);
+const Message = mongoose.model('Message', messageSchema);
+
 let users = {};
 
-app.use(express.static('client/build'));    
+app.use(express.static('client/build'));
 
 io.on('connection', (socket) => {
     console.log('New client connected');
 
-    socket.on('getUsers', () => {
-        const usersList = Object.values(users); 
+    socket.on('getUsers', async () => {
+        const usersList = await User.find();
         socket.emit('users', usersList);
     });
 
-    socket.on('join', (data) => {
+    socket.on('join', async (data) => {
         const { username, profilePicture } = data;
-        users[socket.id] = { username, profilePicture };
-        io.emit('users', Object.values(users));
+        const user = new User({ username, profilePicture });
+        await user.save();
+        io.emit('users', await User.find());
     });
 
-    socket.on('privateMessage', (data) => {
+    socket.on('privateMessage', async (data) => {
         const { recipient, message, sender, timestamp } = data;
+        const roomId = [sender, recipient].sort().join('-');
+        const messageData = new Message({ sender, recipient, message, timestamp, roomId });
+        await messageData.save();
         const recipientSocketId = Object.keys(users).find(key => users[key].username === recipient);
         if (recipientSocketId) {
-            const roomId = [sender, recipient].sort().join('-');
             io.to(recipientSocketId).emit('receiveMessage', { message, sender, roomId, timestamp });
         }
     });
 
-    socket.on('disconnect', () => {
+    socket.on('fetchMessages', async (roomId) => {
+        const messages = await Message.find({ roomId });
+        socket.emit('loadMessages', messages);
+    });
+
+    socket.on('disconnect', async () => {
         const user = users[socket.id];
         if (user) {
             delete users[socket.id];
-            io.emit('users', getUsersList());
+            io.emit('users', await User.find());
             socket.broadcast.emit('userLeft', user.username);
         }
     });
